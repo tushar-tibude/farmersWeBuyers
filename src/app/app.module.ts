ce808e76c06dd8e1483eb950fb3f38a4ea614a71
DESKTOP-DSEPAJ4\Ashish <ashishambule@gmail.com>
2017-09-17 16:08:03 +0530

    1.Added bootstrap 2.Created header, home and contact components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
+
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ContactUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,AlertModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
